2d6bb609bce3046cfa60820347e200ca534a5e68
taplinseth <seth.taplin@g.asutincc.edu>
2021-05-11 19:18:30 -0500

    Question 1

--- src/die.cpp
+++ src/die.cpp
@@ -1,1 +1,14 @@
 //
+#include<die.h>
+
+//randomly assigns a value to roll_value in the range of 1 to 6
+void Die::roll()
+{
+    Die::roll_value = rand() % Die::sides + 1;
+}
+
+//returns roll_value
+int Die::rolled_value() const
+{
+    return Die::roll_value;   
+}
--- src/die.h
+++ src/die.h
@@ -1,1 +1,18 @@
 //
+#ifndef DIE_H
+#define DIE_H
+#include<iostream>
+
+class Die
+{
+public:
+    void roll();
+    int rolled_value() const;
+
+private:
+    int roll_value;
+    int sides{6};
+
+};
+
+#endif
