06b051ed4b991e46b57168a4202b2aca88ca396d
taplinseth <seth.taplin@g.asutincc.edu>
2021-05-11 20:40:39 -0500

    Question 2

--- test/craps_test.cpp
+++ test/craps_test.cpp
@@ -1,18 +1,34 @@
 #define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
 #include "catch.hpp"
 #include "die.h"
+#include "roll.h"
+
 TEST_CASE("Verify Test Configuration", "verification") {
 	REQUIRE(true == true);
 }
 
 TEST_CASE("Tests die class") {
-	Die game;
+	Die die_roll;
 
 	for(int i = 0; i < 10; i++)
 	{
-		game.roll();
-		REQUIRE(game.rolled_value() >= 1);
-		REQUIRE(game.rolled_value() <= 6);
+		die_roll.roll();
+		REQUIRE(die_roll.rolled_value() >= 1);
+		REQUIRE(die_roll.rolled_value() <= 6);
+	}
+
+}
+
+TEST_CASE("Tests roll class") {
+	Die first_die;
+	Die second_die;
+	Roll diceroll(first_die, second_die);
+
+	for(int i = 0; i < 10; i++)
+	{
+		diceroll.roll_die();
+		REQUIRE(diceroll.roll_value() >= 2);
+		REQUIRE(diceroll.roll_value() <= 12);
 	}
 
 }
