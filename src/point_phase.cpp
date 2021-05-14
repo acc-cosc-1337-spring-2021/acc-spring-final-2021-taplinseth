a5d1bb102200c6a723955cbdcfd59bebd9820f0a
taplinseth <seth.taplin@g.asutincc.edu>
2021-05-13 20:18:37 -0500

    Question 4

--- src/point_phase.cpp
+++ src/point_phase.cpp
@@ -1,1 +1,18 @@
 //
+#include "point_phase.h"
+
+RollOutcome PointPhase::get_outcome(Roll* roll)
+{
+    if(roll->roll_value() == PointPhase::point)
+    {
+        return RollOutcome::point;
+    }
+    else if(roll->roll_value() == 7)
+    {
+        return RollOutcome::seven_out;
+    }
+    else
+    {
+        return RollOutcome::nopoint;
+    }
+}
--- test/craps_test.cpp
+++ test/craps_test.cpp
@@ -3,6 +3,9 @@
 #include "die.h"
 #include "roll.h"
 #include "shooter.h"
+#include "come_out_phase.h"
+#include "point_phase.h"
+
 
 TEST_CASE("Verify Test Configuration", "verification") {
 	REQUIRE(true == true);
@@ -43,6 +46,33 @@ TEST_CASE("Tests shooter class, verifies roll result"){
 	{
 		REQUIRE(diceroll.throw_die(first_die, second_die)->roll_value() >= 2);
 		REQUIRE(diceroll.throw_die(first_die, second_die)->roll_value() <= 12);
+	}
+}
+
+TEST_CASE("Tests ComeOutPhase class"){
+	Die first_die;
+	Die second_die;
+	Shooter diceroll;
+	ComeOutPhase come_out;
+
+	for(int i = 0; i < 10; i++)
+	{
+		REQUIRE(come_out.get_outcome(diceroll.throw_die(first_die, second_die)) != RollOutcome::seven_out); //if seven_out then craps, natural, point okay
+	}
+}
+
+TEST_CASE("Test PointPhase"){
+	Die first_die;
+	Die second_die;
+	Shooter diceroll;
+	
+
+	for(int i = 0; i < 10; i++)
+	{
+		int points = rand() % 12 + 1;
+		PointPhase point(points);
+
+		REQUIRE(point.get_outcome(diceroll.throw_die(first_die, second_die)) != RollOutcome::natural); //if natural then seven_out, point, and nopoint okay
 	}
 }
 
